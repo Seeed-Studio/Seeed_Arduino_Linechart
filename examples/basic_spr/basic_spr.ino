31dcbdda3314f4189ac7ccada6d4534d0a14c197
LynnL4 <lht856@foxmail.com>
2022-03-14 16:36:14 +0800

    Add example to use TFT_eSprite, which more smooth

---
+++ examples/basic_spr/basic_spr.ino
@@ -0,0 +1,54 @@
+#include "seeed_line_chart.h" //include the library
+
+TFT_eSPI tft;
+TFT_eSprite spr = TFT_eSprite(&tft); // Sprite
+
+#define max_size 30 // maximum size of data
+doubles data;       // Initilising a doubles type to store data
+int brightness;
+
+void setup()
+{
+    pinMode(A0, INPUT);
+    tft.begin();
+    tft.setRotation(3);
+    spr.createSprite(TFT_HEIGHT, TFT_WIDTH);
+    spr.setRotation(3);
+}
+
+void loop()
+{
+    spr.fillSprite(TFT_WHITE);
+    brightness = analogRead(A0);
+
+    if (data.size() == max_size)
+    {
+        data.pop(); // this is used to remove the first read variable
+    }
+    data.push(brightness); // read variables and store in data
+
+    // Settings for the line graph title
+    auto header = text(0, 0)
+                      .value("Light Sensor Readings")
+                      .align(center)
+                      .valign(vcenter)
+                      .width(spr.width())
+                      .thickness(2);
+
+    header.height(header.font_height(&spr) * 2);
+    header.draw(&spr); // Header height is the twice the height of the font
+
+    // Settings for the line graph
+    auto content = line_chart(20, header.height()); //(x,y) where the line graph begins
+    content
+        .height(spr.height() - header.height() * 1.5) // actual height of the line chart
+        .width(spr.width() - content.x() * 2)         // actual width of the line chart
+        .based_on(0.0)                                // Starting point of y-axis, must be a float
+        .show_circle(false)                           // drawing a cirle at each point, default is on.
+        .value(data)                                  // passing through the data to line graph
+        .color(TFT_RED)                               // Setting the color for the line
+        .draw(&spr);
+
+    spr.pushSprite(0, 0);
+    delay(100);
+}
